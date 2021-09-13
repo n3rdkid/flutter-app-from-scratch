2a0a6a9d9f57aae2d03c857aa837cfe9c0852806
Saurav <sauravads123@gmail.com>
2021-09-13 11:42:48 +0545

    implemented submit answer

--- quiz_riverpod/lib/controllers/quiz_controller.dart
+++ quiz_riverpod/lib/controllers/quiz_controller.dart
@@ -1,6 +1,25 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:quiz_riverpod/controllers/quiz_state.dart';
+import 'package:quiz_riverpod/models/question.dart';
 
 class QuizController extends StateNotifier<QuizState> {
   QuizController() : super(QuizState.initial());
+
+  void submitAnswer(Question currentQuestion, String answer) {
+    if (state.getAnswered) {
+      return;
+    }
+    if (currentQuestion.correctAnswer == answer) {
+      state = state.copyWith(
+          selectedAnswer: answer,
+          correct: state.correct..add(currentQuestion),
+          status: QuizStatus.correct);
+      return;
+    }
+
+    state = state.copyWith(
+        selectedAnswer: answer,
+        correct: state.correct..add(currentQuestion),
+        status: QuizStatus.incorrect);
+  }
 }
