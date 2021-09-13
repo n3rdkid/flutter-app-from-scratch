982611ad6ace8e922cff860817fdf95ee9304a0c
Saurav <sauravads123@gmail.com>
2021-09-13 11:48:58 +0545

    wrap up quiz controller

--- quiz_riverpod/lib/controllers/quiz_controller.dart
+++ quiz_riverpod/lib/controllers/quiz_controller.dart
@@ -2,6 +2,10 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:quiz_riverpod/controllers/quiz_state.dart';
 import 'package:quiz_riverpod/models/question.dart';
 
+final quizControllerProvider =
+    StateNotifierProvider.autoDispose<QuizController>(
+        (ref) => QuizController());
+
 class QuizController extends StateNotifier<QuizState> {
   QuizController() : super(QuizState.initial());
 
@@ -21,5 +25,17 @@ class QuizController extends StateNotifier<QuizState> {
         selectedAnswer: answer,
         correct: state.correct..add(currentQuestion),
         status: QuizStatus.incorrect);
+  }
+
+  void nextQuestion(List<Question> questions, int currentIdx) {
+    state = state.copyWith(
+        selectedAnswer: '',
+        status: currentIdx + 1 < questions.length
+            ? QuizStatus.initial
+            : QuizStatus.complete);
+  }
+
+  void reset() {
+    state = QuizState.initial();
   }
 }
